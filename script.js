fb67faebbe92a93b5da17b2e03311f41d2d98aae
Shira Lotan <shiralotan@h-MacBook-Air-sl-Shira.local>
2019-01-03 15:02:37 +0200

    ToDoMainFile

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+function savemission(){
+    var mission=document.forms["mainBoard"]["mainInput"].value;
+    var date=document.forms["mainBoard"]["dateInput"].value;
+    var time=document.forms["mainBoard"]["timeInput"].value;    
+
+}
